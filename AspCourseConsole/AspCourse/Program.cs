a52a2f334fe66c6e9ff1f9183392149e3300e994
Glebeeezy <daf701@mail.ru>
2023-09-02 17:05:14 +0300

    1

--- AspCourseConsole/AspCourse/Program.cs
+++ AspCourseConsole/AspCourse/Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Linq;
-using System.Text;
-using static System.Net.Mime.MediaTypeNames;
-using System.Text.RegularExpressions;
-
-namespace AspCourse
+﻿namespace AspCourse
 {
     }
     internal class Program
@@ -26,9 +21,10 @@ namespace AspCourse
             {
                 Console.Write(message);
             }
+            Console.WriteLine("");
         }
 
-        Console.Write("Второе задание \nВведите число повторений"); 
+        Console.Write("Второе задание \nВведите число повторений\n"); 
         int number = int.Parse(Console.ReadLine());
         Console.WriteLine("Введите слово");
         string word = Console.ReadLine();
@@ -36,10 +32,75 @@ namespace AspCourse
 
         // Third exercise
         string first = "1 2 3 4 5";
+        string second = "1 2 -3 4 5";
+        string third = "1 9 3 4 -5";
         void HighAndLow (string str)
         {
-            
+            int[] array = str.Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine($"{array.Max()} {array.Min()}");
         }
+        Console.WriteLine("Третье задание");
         HighAndLow(first);
+        HighAndLow(second);
+        HighAndLow(third);
+
+        // Fourth exercise
+        Console.WriteLine("Четвёртое задание");
+        bool isIsogram (string str)
+        {
+            if (str == null)
+            {
+                return true;
+            }
+            else
+            {               
+                str = str.ToLower();
+                int len = str.Length;
+
+                char[] arr = str.ToCharArray();
+
+                Array.Sort(arr);
+                for (int i = 0; i < len - 1; i++)
+                {
+                    if (arr[i] == arr[i + 1])
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        string firstIso = "Dermatoglyphics";
+        string secondIso = "moose";
+        string thirdIso = "aba";
+
+        Console.Write($"{isIsogram(firstIso)}\n{isIsogram(secondIso)}\n{isIsogram(thirdIso)}\n");
+        // Fifth exercise
+        Console.WriteLine("Пятое задание");
+        void accum(string str)
+        {
+            char [] arrayHigh = str.ToUpper().ToCharArray();
+            char [] arrayLow = str.ToLower().ToCharArray();
+
+            for (int i = 0; i < arrayHigh.Length; i++)
+            {
+                if (i > 0 & i < arrayHigh.Length)
+                {
+                    Console.Write("-");
+                }
+                    Console.Write($"{arrayHigh[i]}");
+                for (int x = 1; x <= i; x++)
+                {
+                    Console.Write(arrayLow[i]);
+                }                           
+            }
+            Console.WriteLine();
+        }
+        string accum1 = "abcd";
+        string accum2 = "RqaEzty";
+        string accum3 = "cwAt";
+        accum(accum1);
+        accum(accum2);
+        accum(accum3);
+
     }
 }
